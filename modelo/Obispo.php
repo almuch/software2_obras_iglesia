2e0e0ed25a97a42c7cb72620c9706f8416488fba
Drixmux <ferna.mace.34@gmail.com>
2016-09-13 18:47:05 -0400

    Clase obispo
    Se creo la clase Obispo sin metodos

---
+++ modelo/Obispo.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Fernando
+ * Date: 13/09/2016
+ * Time: 06:46 PM
+ */
+class Obispo{
+    private $db;
+    public function __construct(){
+        $this->db = new Database;
+    }
+}
