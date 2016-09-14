f984337da61af1f18c9ba14725bcfd089bbc9bc6
Drixmux <ferna.mace.34@gmail.com>
2016-09-14 15:44:56 -0400

    headerUser
    Se agregó la cabecera de página para un usuario invitado.

---
+++ map.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: Fernando
+     * Date: 09/14/2016
+     * Time: 3:40 PM
+     */
+    require('core/init.php');
+
+    $template = new Template('templates/map.php');
+
+    echo $template;
+?>
